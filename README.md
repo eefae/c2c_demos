ca2535b8039ee8cf2ffd8b220e62a0e1564e02dc
tunasmoothie <tunasmoothie@gmail.com>
2025-03-21 17:53:40 +0800

    added make functionality and readme update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 WIP
+
+To Restore Vivado Projects:
+1. cd vivado
+
+2. Make usage:
+  make project TARGET=<target>
+  
+  Target List:
+    smf
+    vu13p
+    vu19p
--- vivado/scripts/build.tcl
+++ vivado/scripts/build.tcl
@@ -11,21 +11,35 @@ if {![string equal $ver $version_required]} {
 }
 
 
-create_project -force vu13p ./vu13p -part xcvu13p-fhgb2104-2-e
+# Possible targets & their respective part numbers
+dict set target_dict smf { xczu4eg-fbvb900-2-e }
+dict set target_dict vu13p { xcvu13p-fhgb2104-2-e }
+dict set target_dict vu19p { xilinx.com kc705 { X1 } mb }
 
-add_files -fileset constrs_1 -norecurse {./src/constraints/vu13p.xdc}
-source ./src/bd/vu13p.tcl
 
-make_wrapper -files [get_files vu13p.bd] -top
-add_files -fileset sources_1 -norecurse vu13p/vu13p.gen/sources_1/bd/vu13p/hdl/vu13p_wrapper.v
+# Define variables
+set target [lindex $argv 0]
+set fpga_part [lindex [dict get $target_dict $target] 0]
+
+
+
+
+# Vivado scripts to create project & wrappers
+create_project -force $target $target -part $fpga_part
+add_files -fileset constrs_1 -norecurse ./src/constraints/${target}.xdc
+
+source ./src/bd/${target}.tcl
+
+make_wrapper -files [get_files ${target}.bd] -top
+add_files -fileset sources_1 -norecurse ${target}/${target}.gen/sources_1/bd/${target}/hdl/${target}_wrapper.v
 
 import_files -force
-
 update_compile_order -fileset sources_1
 update_compile_order -fileset constrs_1
 
 close_bd_design [current_bd_design]
-open_bd_design [get_files vu13p.bd]
+open_bd_design [get_files ${target}.bd]
 validate_bd_design -force
 save_bd_design
+
 
